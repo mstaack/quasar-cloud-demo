33410b89ffb3a5005c517d69e37abf87ce2d8f91
mstaack <maxmatteostaack@gmail.com>
2018-02-26 20:05:57 +0100

    add routes for auth

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,18 @@
-
 export default [
   {
     path: '/',
     component: () => import('layouts/default'),
     children: [
-      { path: '', component: () => import('pages/index') }
+      {path: '', component: () => import('pages/index')}
+    ]
+  },
+  {
+    path: '/auth',
+    component: () => import('layouts/auth'),
+    children: [
+      {path: 'login', component: () => import('pages/login')},
+      {path: 'register', component: () => import('pages/register')},
+      {path: 'password-reset', component: () => import('pages/password-reset')}
     ]
   },
 

5fb56529f30bd9c5fed151f999a1534bc20b8d6e
mstaack <maxmatteostaack@gmail.com>
2018-02-26 19:59:08 +0100

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img src="~assets/quasar-logo-full.svg">
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
